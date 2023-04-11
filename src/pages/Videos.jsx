deb91297f4b5347e2b657c7e24b11d6aff1fbf70
sikirha <sikirha@gmail.com>
2023-04-11 13:27:06 +0900

    tanstack & axios for using Mock data

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import VideoCard from "../components/VideoCard";
+import axios from "axios";
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword ? `🔍${keyword}` : "🔥"}</div>;
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], async () => {
+    return axios
+      .get(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((r) => r.data.items);
+  });
+  return (
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : "🔥"}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something wrong!</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
