74200ce4d785422b14ac0839076da7fad7091446
sikirha <sikirha@gmail.com>
2023-04-11 12:07:46 +0900

    seacrch header

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -14,18 +14,19 @@ export default function SearchHeader() {
   }, [keyword]);
   return (
     <header className="flex w-full p-4 text-2xl border-b border-zinc-600 mb-4">
-      <Link to="/">
-        <BsYoutube />
-        <h1>Youtube</h1>
+      <Link to="/" className="flex items-center">
+        <BsYoutube className="text-4xl text-brand" />
+        <h1 className="font-bold ml-2 text-3xl">Youtube</h1>
       </Link>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="w-full flex justify-center">
         <input
           type="text"
           placeholder="Search..."
           value={text}
           onChange={(e) => SetText(e.target.value)}
+          className="w-7/12 p-2 outline-none bg-black text-gray-50"
         />
-        <button>
+        <button className="bg-zinc-600 px-4">
           <BsSearch />
         </button>
       </form>
