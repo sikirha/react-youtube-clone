e135a62f48acfd7304e4a80b5d604221c089ec93
sikirha <sikirha@gmail.com>
2023-04-11 16:59:11 +0900

    Context for changing API

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,20 +2,19 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import VideoCard from "../components/VideoCard";
-import { search } from "../api/youtube";
-import FakeYoutube from "../api/fakeYoutube";
-import Youtube from "../api/youtube";
+// import { search } from "../api/youtube";
+// import FakeYoutube from "../api/fakeYoutube";
+// import Youtube from "../api/youtube";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
   const {
     isLoading,
     error,
     data: videos,
-  } = useQuery(["videos", keyword], () => {
-    const youtube = new FakeYoutube();
-    return youtube.search(keyword);
-  });
+  } = useQuery(["videos", keyword], () => youtube.search(keyword));
   return (
     <>
       <div>Videos {keyword ? `🔍${keyword}` : "🔥"}</div>
